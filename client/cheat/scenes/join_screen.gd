9cfba73519ddc93b6d4621a3d3caf2e24c95fc9b
Sanuj Syal <sanujsyal@gmail.com>
2025-02-12 15:34:35 -0500

    Paste join code button with appropriate icon

--- client/cheat/scenes/join_screen.gd
+++ client/cheat/scenes/join_screen.gd
@@ -4,7 +4,7 @@ extends Control
 
 
 func submit() -> void:
-	Globals.join_code = join_input.get_text()
+	Globals.join_code = join_input.get_text().strip_edges()
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
@@ -14,3 +14,6 @@ func _on_join_input_text_submitted(new_text: String) -> void:
 
 func _on_submit_join_pressed() -> void:
 	submit()
+
+func _on_paste_join_code_pressed() -> void:
+	join_input.set_text(DisplayServer.clipboard_get().strip_edges())
